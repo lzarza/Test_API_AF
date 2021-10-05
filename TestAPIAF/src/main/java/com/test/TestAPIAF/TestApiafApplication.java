0e2d8dc54ff1e9a01cccfc37f06bbbc518d8b85e
lzarzadlg <l.zarza@dlgworld.com>
2021-10-05 17:32:12 +0200

    Initial import

---
+++ TestAPIAF/src/main/java/com/test/TestAPIAF/TestApiafApplication.java
@@ -0,0 +1,13 @@
+package com.test.TestAPIAF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApiafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApiafApplication.class, args);
+	}
+
+}
